b4d84ce0e9c6ed8fb66c32affabdb0f4562eb0cd
김광철 <anyman@afreecatv.com>
2017-01-23 20:10:59 +0900

    first commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $posts = \App\Post::with('user')->paginate(3);
+
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $post = new \App\Post;
+        return view('posts.create', compact('post'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        //
+        $post = $request->user()
+            ->Posts()
+            ->create($request->all());
+
+
+        return redirect(route('posts.show', $post->id));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        $post->load('user');
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+        //권한이 없으며 뒤로
+        $this->authorize('update', $post);
+
+
+        return view('posts.edit',compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+
+        //권한이 없으며 뒤로
+        $this->authorize('update', $post);
+
+
+        $post->update($request->all());
+        return redirect(route('posts.show',$post->id));
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+        //권한이 없으며 뒤로
+        $this->authorize('delete', $post);
+
+        $post->delete();
+
+        return response()->json([],204);
+    }
+}
---
+++ app/helpers.php
@@ -0,0 +1,7 @@
+<?php
+
+function markdown($markdown)
+{
+    return   app(ParsedownExtra::class)->text($markdown);
+
+}
